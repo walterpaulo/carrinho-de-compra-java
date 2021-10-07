b1c50a1ae8d6b077dbf408d18639588da28bb389
walter <walter0paulo@hotmail.com>
2021-10-07 15:51:09 -0300

    Preparando camanda resource, API

---
+++ src/main/java/br/com/supera/api/SpringBootApiUsApplication.java
@@ -0,0 +1,13 @@
+package br.com.supera.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiUsApplication.class, args);
+	}
+
+}
