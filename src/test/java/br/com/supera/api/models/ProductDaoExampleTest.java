a0f6b4bf755ec6af8dad795b2cf5b1ebc384552c
walter <walter0paulo@hotmail.com>
2021-10-08 11:33:37 -0300

    Adicionado test, quantidadeItemNoCarrinho_test

--- src/test/java/br/com/supera/api/models/ProductDaoExampleTest.java
+++ src/test/java/br/com/supera/api/models/ProductDaoExampleTest.java
@@ -11,14 +11,16 @@ import com.github.dbunit.rules.api.configuration.DBUnit;
 import com.github.dbunit.rules.api.dataset.DataSet;
 import com.github.dbunit.rules.util.EntityManagerProvider;
 
+import br.com.supera.api.services.CarrinhoDeCompra;
+import br.com.supera.api.services.CriadorDeCarrinho;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import static com.github.dbunit.rules.util.EntityManagerProvider.em;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
@@ -26,12 +28,33 @@ import static org.junit.Assert.fail;
 
 @RunWith(JUnit4.class)
 public class ProductDaoExampleTest {
+	
+	public CarrinhoDeCompra getCompra() {
+		return compra;
+	}
+
+	public void setCompra(CarrinhoDeCompra compra) {
+		this.compra = compra;
+	}
+	private CarrinhoDeCompra compra;
+	
+	
 
     @Rule
     public EntityManagerProvider emProvider = EntityManagerProvider.instance("productDS");  
 
     @Rule
 	public DBUnitRule dbUnitRule = DBUnitRule.instance(emProvider.connection()); 
+    
+    @Before
+	public void iniciarCenario() {
+		CriadorDeCarrinho criador = new CriadorDeCarrinho();
+		CarrinhoDeCompra carrinho = criador
+				.constroi();
+		setCompra(carrinho);
+
+	}
+    
 	
 	@Test
 	@DBUnit(allowEmptyFields = true)
@@ -64,6 +87,40 @@ public class ProductDaoExampleTest {
 			
 			assertNotNull(product);
 			assertEquals(obj.getName(), product.getName());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+        
+    }
+	@Test
+	@DBUnit(allowEmptyFields = true)
+    @DataSet("products.yml") 
+    public void adicionarProduto_test() {
+		try {
+			
+			Product obj = new Product(99L, "Call Of Duty WWII", BigDecimal.valueOf(249.99), Short.parseShort("205"), "call-of-duty-wwii.png");
+			Product product =  (Product) em(). 
+					createQuery("select p from Product p where p.id = :pid")
+					.setParameter("pid", 99L)
+					.getSingleResult();
+			
+			assertNotNull(product);
+			assertEquals(obj.getName(), product.getName());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+        
+    }
+	
+	@Test
+	@DBUnit(allowEmptyFields = true)
+    @DataSet("products.yml") 
+    public void quantidadeItemNoCarrinho_test() {
+		try {
+			assertNotNull(getCompra());
+			assertEquals(0, getCompra().quantidadeProdutos());
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail(e.getMessage());
