857ce20e54537dd7ab74fb0d92537c60cfad5003
walter <walter0paulo@hotmail.com>
2021-10-08 10:57:23 -0300

    Busca no banco por id, comparação final por nome-Test

--- src/test/java/br/com/supera/api/models/ProductDaoExampleTest.java
+++ src/test/java/br/com/supera/api/models/ProductDaoExampleTest.java
@@ -1,6 +1,10 @@
 package br.com.supera.api.models;
 
+import java.math.BigDecimal;
 import java.util.List;
+
+import javax.persistence.Parameter;
+import javax.persistence.Query;
 
 import com.github.dbunit.rules.DBUnitRule;
 import com.github.dbunit.rules.api.configuration.DBUnit;
@@ -14,6 +18,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import static com.github.dbunit.rules.util.EntityManagerProvider.em;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
@@ -38,6 +43,27 @@ public class ProductDaoExampleTest {
 					getResultList();
 			assertNotNull(products);
 			assertEquals(9, products.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+        
+    }
+	
+	@Test
+	@DBUnit(allowEmptyFields = true)
+    @DataSet("products.yml") 
+    public void procurarProdutoPorId_test() {
+		try {
+			
+			Product obj = new Product(99L, "Call Of Duty WWII", BigDecimal.valueOf(249.99), Short.parseShort("205"), "call-of-duty-wwii.png");
+			Product product =  (Product) em(). 
+					createQuery("select p from Product p where p.id = :pid")
+					.setParameter("pid", 99L)
+					.getSingleResult();
+			
+			assertNotNull(product);
+			assertEquals(obj.getName(), product.getName());
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail(e.getMessage());
