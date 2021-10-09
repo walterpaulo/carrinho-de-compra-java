e227387d4b21fd01e34a4883aec04da1bbd7e7b0
walter <walter0paulo@hotmail.com>
2021-10-09 18:54:46 -0300

    Lista de produto, e praparando carrinho de compra.

--- src/main/java/br/com/supera/api/resources/CarrinhoResource.java
+++ src/main/java/br/com/supera/api/resources/CarrinhoResource.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.supera.api.models.Product;
+import br.com.supera.api.services.CarrinhoDeCompra;
 import br.com.supera.api.services.CriadorDeCarrinho;
 import br.com.supera.api.services.ProdutoService;
 
@@ -17,12 +19,25 @@ import br.com.supera.api.services.ProdutoService;
 public class CarrinhoResource {
 
 	private final String PAGINA_LISTA = "listaProduto";
-	private final String PAGINA_CARRINHO = "listaCarrinho";
+	private final String PAGINA_CARRINHO = "carrinhoDeProdutos";
+	
+	private CarrinhoDeCompra compra;
+	private final CriadorDeCarrinho criador = new CriadorDeCarrinho();
+	private CarrinhoDeCompra carrinho;
+
+	public CarrinhoDeCompra getCompra() {
+		return compra;
+	}
+
+	public void setCompra(CarrinhoDeCompra compra) {
+		this.compra = compra;
+	}
+	
 
 	@Autowired
 	private ProdutoService service;
 
-	@GetMapping
+	@GetMapping("")
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView(PAGINA_LISTA);
 		List<Product> produtos = service.getProduto();
@@ -30,5 +45,17 @@ public class CarrinhoResource {
 		mv.addObject("produtos", produtos);
 		return mv;
 	}
+	
+//	@GetMapping(value = "/adicionar/{id}")
+//	public ModelAndView adicionar(@PathVariable("id") Long id) {
+//		Product produto = this.service.findPorId(id).orElseThrow(()-> new EmptyResultDataAccessException(1));
+//		ModelAndView mv = new ModelAndView(PAGINA_CARRINHO);
+//		carrinho = criador.pegarProduto(produto, 1).constroi();
+//		setCompra(carrinho);		
+//		mv.addObject("produtos", getCompra());
+//		return mv;
+//	}
+	
+	
 
 }
